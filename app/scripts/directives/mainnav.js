387033dc17da123a7b0b957647446ddc3b31a638
xingbo828 <xingbo828@gmail.com>
2014-08-07 21:48:20 +0000

    fix nav

--- app/scripts/directives/mainnav.js
+++ app/scripts/directives/mainnav.js
@@ -5,7 +5,7 @@
  * @description
  * # mainNav
  */
-angular.module('appointmeApp').directive('mainNav', function ($location, Auth) {
+angular.module('appointmeApp').directive('mainNav', function ($location, Auth, $document, Utility) {
     return {
         templateUrl: 'scripts/directives/partials/mainNav.html',
         restrict: 'E',
@@ -35,6 +35,41 @@ angular.module('appointmeApp').directive('mainNav', function ($location, Auth) {
             });
             scope.$on('logged_out', function () {
                 scope.isAuthed = false;
+            });
+            
+            scope.isIconMenuOpen = false;
+            
+            scope.isMobile = Utility.isMobile();
+            if(!scope.isMobile){
+                scope.isIconMenuOpen = true;
+            }
+            
+            scope.isMenuOpen = false;
+            var setIconMenuOpen = function (open) {
+                scope.isIconMenuOpen = !scope.isMobile && open;
+            };
+            scope.openMenu = function () {
+                scope.isMenuOpen = true;
+            };
+            scope.toggleMenu = function ($event) {
+                if( !! $event) {
+                    $event.stopPropagation();
+                }
+                scope.isMenuOpen = !scope.isMenuOpen;
+            };
+//             scope.openIconMenu = function () {
+//                 setIconMenuOpen(true);
+//             };
+            scope.closeMenu = function () {
+                scope.isMenuOpen = false;
+            };
+//             scope.closeIconMenu = function () {
+//                 scope.isIconMenuOpen = false;
+//                 setIconMenuOpen(false);
+//             };
+            $document.click(function () {
+                scope.closeMenu();
+                scope.$digest();
             });
         }
     };
